0c991b41cd6a39456b69ffa33b772d4d4c5736f9
Apple <apple@Apples-MacBook-Pro-2.local>
2019-02-19 15:44:19 +0530

    Initial Commit

---
+++ Alhisba/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Alhisba
+//
+//  Created by Apple on 19/02/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
